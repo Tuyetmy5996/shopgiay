68f285f58c3e0688c194175d9da18356740b561b
Tuyetmy5996 <myntt5996@gmail.com>
2017-06-25 08:04:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = '../shoponline/system';
+	$system_path = '../shopgiay/system';
 
 /*
  *---------------------------------------------------------------
@@ -113,7 +113,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '../shoponline/application';
+	$application_folder = '../shopgiay/application';
 
 /*
  *---------------------------------------------------------------
